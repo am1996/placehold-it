f71e4e8e836f62630334edb071bf82b73caefbf2
7 <ahmedmagdy92@yahoo.com>
2018-03-10 13:09:54 +0200

    domain allowed

--- placeholdit/settings.py
+++ placeholdit/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ['SECRET']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["placehold-it.herokuapp.com"]
 
 
 # Application definition
